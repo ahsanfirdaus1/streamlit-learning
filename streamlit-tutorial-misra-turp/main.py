afe12a2a42a26f97c73c87d4ee2a4019ed8337c4
Ahsan <78904717+ahsanfirdaus1@users.noreply.github.com>
2024-03-16 12:54:25 +0700

    on going

--- streamlit-tutorial-misra-turp/main.py
+++ streamlit-tutorial-misra-turp/main.py
@@ -3,16 +3,26 @@ import streamlit as st
 header = st.container()
 dataset = st.container()
 features = st.container()
-modeltraining = st.container()
+model_training = st.container()
 
 with header:
     st.title('Welcome to my awsome data science project!')
+    st.text('In this project I look into the transactions of taxis in NYC. ...')
 
 with dataset:
+    st.header('NYC taxi dataset')
+    st.text('I found this dataset on kaggle.com')
 
 
 with features:
+    st.header('The features I created')
+    
 
+
+with model_training:
+    st.header('Time to train the model!')
+    st.text('Here you get to choose the hyperparameters of the model and see how the performance changes!')
+    
 
 
 
