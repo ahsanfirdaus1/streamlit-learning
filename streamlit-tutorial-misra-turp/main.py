30fa9cdbfad07d5826863e3f477dc17387974c0a
Ahsan <78904717+ahsanfirdaus1@users.noreply.github.com>
2024-03-16 14:38:50 +0700

    adding columns and its content like user input

--- streamlit-tutorial-misra-turp/main.py
+++ streamlit-tutorial-misra-turp/main.py
@@ -35,6 +35,17 @@ with model_training:
     st.header('Time to train the model!')
     st.text('Here you get to choose the hyperparameters of the model and see how the performance changes!')
 
+    sel_col, disp_col = st.columns(2)
+
+    max_depth = sel_col.slider('What should be the max_depth of the model?', min_value=10, max_value=100, value=30, step=10)
+
+    n_estimators = sel_col.selectbox('Hpw many trees should there be?', options=[100, 200,300,'No limit'], index = 0)
+
+    input_feature = sel_col.text_input('Which feature should be used as the input feature?', 'Num of pesanger')
+
+
+
+
 
 
 
