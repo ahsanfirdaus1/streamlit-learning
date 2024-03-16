e62c73e8c4760fab1401437fe8d8a6e1a1213c20
Ahsan <78904717+ahsanfirdaus1@users.noreply.github.com>
2024-03-16 12:13:40 +0700

    Working on my first tutorial of streamlit (unfinished)

--- streamlit-tutorial-misra-turp/main.py
+++ streamlit-tutorial-misra-turp/main.py
@@ -1,6 +1,12 @@
 import steamlit as st
 
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+modeltraining = st.beta_container()
 
+with header:
+    st.Title('Welcome to my awsome data science project!')
 
 
 
