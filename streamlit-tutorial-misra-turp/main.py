a8ff12b95a349d4d46bc07d7b342aef1f49bdd26
Ahsan <78904717+ahsanfirdaus1@users.noreply.github.com>
2024-03-16 14:30:01 +0700

    adjustment on dataset path on linux ubuntu

--- streamlit-tutorial-misra-turp/main.py
+++ streamlit-tutorial-misra-turp/main.py
@@ -14,7 +14,8 @@ with dataset:
     st.header('NYC taxi dataset')
     st.text('I found this dataset on kaggle.com')
 
-    taxi_data = pd.read_csv('dataset\\taxi\\taxi_fare\\train.csv')
+    #taxi_data = pd.read_csv('dataset\\taxi\\taxi_fare\\train.csv')
+    taxi_data = pd.read_csv('dataset/taxi/taxi_fare/train.csv')
     st.write(taxi_data.head(20))
 
     st.subheader('Passengers Amount Distribution')
